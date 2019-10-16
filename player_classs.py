961449260b0a221bf2902cda73b42cb81066bbc4
lorygroppo <lorenzogroppo96@gmail.com>
2019-10-16 15:31:07 +0200

    v0.1
    Project creation

---
+++ player_classs.py
@@ -0,0 +1,4 @@
+class player:
+    def __init__(self, nome, numero):
+        self.nome = nome
+        self.numero = numero
---
+++ stat_class.py
@@ -0,0 +1,27 @@
+class stats:
+    def __init__(self, partita, data): #costruttore
+        self.partita = partita
+        self.data = data
+    
+    def save_stats(line):
+        
+        #codice per decodifica della line
+
+        self.T2r =
+        self.T2t =
+        self.T3r =
+        self.T3t =
+        self.TLr = 
+        self.TLt =
+        self.PT = self.TLr + 2*self.T2r + 3*self.T3r
+        self.AST =
+        self.RD =
+        self.RO =
+        self.RT = self.RD + self.RO
+        self.PR =
+        self.PP =
+        self.FF =
+        self.FS =
+        self.STP =
+        self.MIN =
+        self.VAL = 
