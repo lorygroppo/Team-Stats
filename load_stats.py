a1b765f6c15cd5e092989f47e808ed1c9f23ce6e
lorygroppo <lorenzogroppo96@gmail.com>
2019-10-17 00:13:27 +0200

    update load function

--- load_stats.py
+++ load_stats.py
@@ -1,7 +1,9 @@
 f = open("ex.txt","r")
 info = f.readline() #legge la prima riga contenente le informazioni della partita
 fields = f.readline().strip().split(",") #prelevo i nomi dei campi dalla seconda riga
-team = []
+team = {}
+
+#leggo righe restanti, relative ad ogni giocatore
 for line in f.readlines():
     print(line)
     value = line.strip().split(",")
@@ -11,7 +13,10 @@ for line in f.readlines():
     if (len(value) == len(fields)):
         for i in range(len(value)):
             giocatore[fields[i]] = value[i]
-        team.append(giocatore)
+        try giocatore.pop("NOME")
+        else:
+            print "ERRORE: campo NOME non corretto"
+        print(giocatore)
     else:
         print("ERRORE NEL NUMERO DI CAMPI")
 print(team)
